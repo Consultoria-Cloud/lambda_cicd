fb7059079655eec73ee6d945642f0ef03a083a45
jonathanalves90 <jonathan.alves90@outlook.com>
2024-07-01 17:27:49 -0300

    Version 10

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda! - V1.0.0.6 - Version Lambda 1.0')
+        'body': json.dumps('Hello from Lambda! - V1.0.0.10 - Version Lambda 10')
     }
